396d4e0f5573fd70961f9b8d28ce685964310b85
Luis D'Avoglio Zanetta <luis.dzanetta@hotmail.com>
2025-03-08 22:48:47 -0300

    Create create_model.sql

---
+++ predict_visitor_purchases/create_model.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE MODEL `google_ml_predict_visitor_purchases.sample_model`
+OPTIONS(model_type='logistic_reg') AS
+SELECT
+  *
+FROM
+  `google_ml_predict_visitor_purchases.training_data`;
